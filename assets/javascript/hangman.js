c24b624d2fb89e1336b8b58910df1e7804149acd
August Conwell <august.conwell@gmail.com>
2017-02-19 15:58:01 -0500

    Fixed '.' and ',' but in highlighting the paragraph

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,4 +1,4 @@
-		var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie condimentum dictum. Etiam condimentum placerat semper. Donec non mauris a est pretium rutrum. Pellentesque molestie massa vitae felis auctor luctus. Donec fermentum dapibus dui, ut pharetra urna ullamcorper ut. Duis ac tempus augue. Proin eu pellentesque risus. Integer posuere pharetra blandit. Nunc dolor tellus, placerat in massa et, posuere consequat nibh. Vestibulum ex diam, elementum quis blandit in, imperdiet id neque. <br> Sed sed sapien vel est iaculis vulputate eu nec nisl. Cras luctus porta accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla interdum ut sapien vel fermentum. Ut non nulla quis massa auctor condimentum. Aenean rhoncus risus eget erat tempus tristique eu sed orci. Sed in sem in metus vehicula commodo sed sit amet neque. Proin semper scelerisque velit, ut porta lectus bibendum sed. Ut bibendum dignissim tellus, eu aliquam libero dapibus eu. Fusce non nibh nisl. Ut pharetra placerat ex, non tempor orci mattis at. <br> Sed non lorem tempor, eleifend augue a, luctus leo. Curabitur a vulputate elit. Mauris in malesuada felis, id vestibulum libero. Donec imperdiet turpis in est posuere, eu elementum tortor pellentesque. Donec blandit, dolor sit amet dapibus auctor, dolor neque aliquam dui, sodales dignissim tortor magna eu dui. Nam vel finibus nisl, non maximus lorem. Sed molestie felis eros, non sodales lectus bibendum a. Suspendisse venenatis lacus sodales scelerisque rhoncus. Phasellus nulla risus, consequat in velit eu, lobortis semper nisi. Pellentesque facilisis mauris non enim cursus, et sollicitudin lorem lacinia. Quisque fringilla risus ut commodo feugiat. Praesent eu leo et tortor rhoncus convallis pharetra quis risus. Aenean est felis, gravida quis elit vel, tincidunt viverra ligula.";
+		var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie condimentum dictum. Etiam condimentum placerat semper. Donec non mauris a est pretium rutrum. Pellentesque molestie massa vitae felis auctor luctus. Donec fermentum dapibus dui, ut pharetra urna ullamcorper ut. Duis ac tempus augue. Proin eu pellentesque risus. Integer posuere pharetra blandit. Nunc dolor tellus, placerat in massa et, posuere consequat nibh. Vestibulum ex diam, elementum quis blandit in, imperdiet id neque. <br> <br> Sed sed sapien vel est iaculis vulputate eu nec nisl. Cras luctus porta accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla interdum ut sapien vel fermentum. Ut non nulla quis massa auctor condimentum. Aenean rhoncus risus eget erat tempus tristique eu sed orci. Sed in sem in metus vehicula commodo sed sit amet neque. Proin semper scelerisque velit, ut porta lectus bibendum sed. Ut bibendum dignissim tellus, eu aliquam libero dapibus eu. Fusce non nibh nisl. Ut pharetra placerat ex, non tempor orci mattis at. <br> <br> Sed non lorem tempor, eleifend augue a, luctus leo. Curabitur a vulputate elit. Mauris in malesuada felis, id vestibulum libero. Donec imperdiet turpis in est posuere, eu elementum tortor pellentesque. Donec blandit, dolor sit amet dapibus auctor, dolor neque aliquam dui, sodales dignissim tortor magna eu dui. Nam vel finibus nisl, non maximus lorem. Sed molestie felis eros, non sodales lectus bibendum a. Suspendisse venenatis lacus sodales scelerisque rhoncus. Phasellus nulla risus, consequat in velit eu, lobortis semper nisi. Pellentesque facilisis mauris non enim cursus, et sollicitudin lorem lacinia. Quisque fringilla risus ut commodo feugiat. Praesent eu leo et tortor rhoncus convallis pharetra quis risus. Aenean est felis, gravida quis elit vel, tincidunt viverra ligula.";
 		var loremArray = [];
 		var loremFullArray = [];
 		var wordBank = ["test", "example"]; // Words to pick from
@@ -16,10 +16,11 @@
 		var announcementSpan = document.getElementById('announcement');
 		var loremDiv = document.getElementById("lorem")
 
-		// mixWordBank();
 
 		filterLoremArray();
 
+		mixWordBank();
+		
 		getFirstWord();
 
 		pickWord();
@@ -39,15 +40,7 @@
 
 		// function declarations
 		
-		// function mixWordBank() { // mixes the wordBank so it is different every page visit
-		// 	var mixedBank = [];
-		// 	var length = wordBank.length;
-		// 	for (var i = 0; i < length; i++) {
-		// 		var randomIndex = Math.floor(Math.random() * wordBank.length);
-		// 		mixedBank.push(wordBank.splice(randomIndex, 1)[0]);
-		// 	}
-		// 	wordBank = mixedBank;
-		// }
+
 		function filterLoremArray() {
 			loremArray = lorem.split(" ")
 			for (var i = 0; i < loremArray.length; i++) {
@@ -71,8 +64,17 @@
 			}
 		}
 
+		function mixWordBank() { // mixes the wordBank so it is different every page visit
+			var mixedBank = [];
+			var length = loremArray.length;
+			for (var i = 0; i < length; i++) {
+				var randomIndex = Math.floor(Math.random() * loremArray.length);
+				mixedBank.push(loremArray.splice(randomIndex, 1)[0]);
+			}
+			wordBank = mixedBank;
+		}
+		
 		function getFirstWord() {
-			wordBank = loremArray;
 			wordIndex = Math.floor(Math.random() * loremArray.length);
 		};
 
@@ -82,7 +84,8 @@
 			hintArray = [];
 			guesses = 9;
 			guessSpan.innerHTML = 9;
-			announcementSpan.innerHTML = ""
+			announcementSpan.innerHTML = "";
+			fadeLorem();
 
 			if (wordIndex < wordBank.length - 1) {
 				wordIndex++;
@@ -151,7 +154,7 @@
 			wins++;
 			winSpan.innerHTML = wins;
 			showLorem();
-			announcementSpan.innerHTML = "Magnificum! P ress 1 to play again";
+			announcementSpan.innerHTML = "Magnificum! Press 1 to play again";
 			reset = true;
 		}
 
@@ -164,13 +167,23 @@
 		function showLorem() {
 			loremFullArray = lorem.split(" ");
 			for (var i = 0; i < loremFullArray.length; i++) {
-				if (loremFullArray[i] === currentWord) {
+				var thisWord = loremFullArray[i]
+				var lastChar = thisWord[thisWord.length - 1];
+				var shorter = loremFullArray[i].slice(0, -1);
+				if (loremFullArray[i].toLowerCase() === currentWord || shorter === currentWord) {
 					loremFullArray.splice(i, 0, "<strong>");
-					loremFullArray.splice(i +2, 0, "</strong>")
+					spaceRemove = loremFullArray[i].length;
+					loremFullArray[i + 1] += "</strong> ";
 					i++;
 				}
 			}
 			loremDiv.innerHTML = "<p>" + loremFullArray.join(" ");
+			//fade in
+			loremDiv.className = "fadeIn";
+		}
+
+		function fadeLorem() { //fade out 
+			loremDiv.className = "fadeOut"
 		}
 
 		function nextGame() {
