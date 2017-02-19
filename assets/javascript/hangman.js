6732022542e61daed233a179323e282ab0d66d90
August Conwell <august.conwell@gmail.com>
2017-02-19 14:45:46 -0500

    Refined formatting, added lorem paragrphs

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,5 +1,6 @@
 		var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie condimentum dictum. Etiam condimentum placerat semper. Donec non mauris a est pretium rutrum. Pellentesque molestie massa vitae felis auctor luctus. Donec fermentum dapibus dui, ut pharetra urna ullamcorper ut. Duis ac tempus augue. Proin eu pellentesque risus. Integer posuere pharetra blandit. Nunc dolor tellus, placerat in massa et, posuere consequat nibh. Vestibulum ex diam, elementum quis blandit in, imperdiet id neque. <br> Sed sed sapien vel est iaculis vulputate eu nec nisl. Cras luctus porta accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla interdum ut sapien vel fermentum. Ut non nulla quis massa auctor condimentum. Aenean rhoncus risus eget erat tempus tristique eu sed orci. Sed in sem in metus vehicula commodo sed sit amet neque. Proin semper scelerisque velit, ut porta lectus bibendum sed. Ut bibendum dignissim tellus, eu aliquam libero dapibus eu. Fusce non nibh nisl. Ut pharetra placerat ex, non tempor orci mattis at. <br> Sed non lorem tempor, eleifend augue a, luctus leo. Curabitur a vulputate elit. Mauris in malesuada felis, id vestibulum libero. Donec imperdiet turpis in est posuere, eu elementum tortor pellentesque. Donec blandit, dolor sit amet dapibus auctor, dolor neque aliquam dui, sodales dignissim tortor magna eu dui. Nam vel finibus nisl, non maximus lorem. Sed molestie felis eros, non sodales lectus bibendum a. Suspendisse venenatis lacus sodales scelerisque rhoncus. Phasellus nulla risus, consequat in velit eu, lobortis semper nisi. Pellentesque facilisis mauris non enim cursus, et sollicitudin lorem lacinia. Quisque fringilla risus ut commodo feugiat. Praesent eu leo et tortor rhoncus convallis pharetra quis risus. Aenean est felis, gravida quis elit vel, tincidunt viverra ligula.";
 		var loremArray = [];
+		var loremFullArray = [];
 		var wordBank = ["test", "example"]; // Words to pick from
 		var wordIndex = 0;
 		var currentWord = wordBank[wordIndex];
@@ -13,6 +14,7 @@
 		var guessSpan = document.getElementById('guesses');
 		var letterSpan = document.getElementById('letters');
 		var announcementSpan = document.getElementById('announcement');
+		var loremDiv = document.getElementById("lorem")
 
 		// mixWordBank();
 
@@ -32,6 +34,7 @@
 					testLetter(event.key);
 				}
 			}
+
 		};
 
 		// function declarations
@@ -86,7 +89,7 @@
 			} else {
 				wordIndex = 0;
 			}
-			currentWord = wordBank[wordIndex];
+			currentWord = wordBank[wordIndex].toLowerCase();
 			for (var i = 0; i < currentWord.length; i++) {
 				hintArray.push("_");
 			}
@@ -147,14 +150,28 @@
 		function youWin() {
 			wins++;
 			winSpan.innerHTML = wins;
-			announcementSpan.innerHTML = "Yay! press 1 to play again";
+			showLorem();
+			announcementSpan.innerHTML = "Magnificum! P ress 1 to play again";
 			reset = true;
 		}
 
 		function youLose() {
+			showLorem();
 			announcementSpan.innerHTML = "Uh-oh, it was " + currentWord + "! Press 1 to play again";
 			reset = true
 		};
+
+		function showLorem() {
+			loremFullArray = lorem.split(" ");
+			for (var i = 0; i < loremFullArray.length; i++) {
+				if (loremFullArray[i] === currentWord) {
+					loremFullArray.splice(i, 0, "<strong>");
+					loremFullArray.splice(i +2, 0, "</strong>")
+					i++;
+				}
+			}
+			loremDiv.innerHTML = "<p>" + loremFullArray.join(" ");
+		}
 
 		function nextGame() {
 			pickWord();
